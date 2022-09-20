a08ac275eb586b97dbcffab7355d7f68fe37f47b
Junior_Trevisol <jr.trevis@hotmail.com>
2022-09-20 13:17:58 +0100

    first commit

---
+++ modelo/assets/js/main.js
@@ -0,0 +1,72 @@
+// Capturar evento de submit do formulario
+
+const form = document.querySelector('#form')
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso) {
+        setResultado('Peso Invalido', false);
+        return;
+    }
+
+    if (!altura) {
+        setResultado('Altura Invalido', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    const msg = `Seu IMC eh ${imc} (${nivelImc}).`;
+    setResultado(msg, true);
+
+});
+
+
+function getNivelImc(imc) {
+    const nivel = ['Abaixo peso', 'Peso Normal', 'Sobrepeso',
+        'Obesidade Grau 1', 'Obesidade Grau 2', 'Obesidade Grau 3'];
+
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
+}
+
+
+function criaP() {
+    const p = document.createElement('p');
+    return p;
+}
+
+
+function setResultado(msg, isValid) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+
+    const p = criaP();
+
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+
+
+    p.innerHTML = msg
+    resultado.appendChild(p);
+
+}
+
+function getImc(peso, altura) {
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
